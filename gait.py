2318c44bdb6bc88b4aa3327590ae8f0f0e9e82b3
ryanarjun <karshan813@gmail.com>
2018-09-17 12:06:59 -0400

    Added the gaitData.csv

--- gait.py
+++ gait.py
@@ -7,7 +7,7 @@ x_values = []
 y_values = []
 z_values = []
 
-with open('hw4_gaitData.csv') as csv_file:
+with open('gaitData.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
